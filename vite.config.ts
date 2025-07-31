de209fef2cb07d7cb204aa1b4f68f5ad609f1c44
book_cnk_ <bookchanakarn@gmail.com>
2025-07-31 18:22:52 +0700

    Add base path configuration in Vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/gradkmutt/",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // 👉 ให้ @ ชี้ไปยัง src
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
